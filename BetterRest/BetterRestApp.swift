9387cd7493b77f6889d57bfe79dd22d7a3902274
lysonjeada <lyscalefi@gmail.com>
2023-12-03 22:40:08 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Amaryllis Rosemaria Baldrez Calefi on 03/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
